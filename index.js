232f167f0b424e7cf7bcfc00581b1e85c4d19d1f
Dmytro Stoianov <dmytrostoianov05@gmail.com>
2025-03-11 23:22:57 +0100

    Implement basic category scraping

---
+++ index.js
@@ -0,0 +1,62 @@
+const puppeteer = require("puppeteer")
+
+async function scrapeJumboOffers() {
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+  )
+
+  await page.goto("https://www.jumbo.com/aanbiedingen/nu", {
+    waitUntil: "networkidle2",
+  })
+
+  let previousHeight = 0
+  while (true) {
+    let newHeight = await page.evaluate(() => document.body.scrollHeight)
+    if (newHeight === previousHeight) break
+    previousHeight = newHeight
+    await page.evaluate("window.scrollTo(0, document.body.scrollHeight)")
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+  }
+
+  const categories = await page.evaluate(() => {
+    const categoryElements = document.querySelectorAll(
+      ".category-section.padding-bottom"
+    )
+    const categoryData = []
+
+    categoryElements.forEach((categoryElement) => {
+      const categoryTitle = categoryElement
+        .querySelector(".category-heading strong")
+        ?.innerText.trim()
+
+      const offers = []
+      const offerElements = categoryElement.querySelectorAll("article")
+
+      offerElements.forEach((offerElement) => {
+        const content = offerElement
+          .querySelector(".content h3")
+          ?.innerText.trim()
+        const imageUrl = offerElement.querySelector(".card-image img")?.src
+
+        offers.push({ image: imageUrl, content })
+      })
+
+      categoryData.push({ name: categoryTitle, offers })
+    })
+
+    return categoryData
+  })
+
+  await browser.close()
+  return categories
+}
+
+async function main() {
+  const jumboOffers = await scrapeJumboOffers()
+  console.log(JSON.stringify(jumboOffers, null, 2))
+}
+
+main()
